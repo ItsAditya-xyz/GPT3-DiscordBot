71110c6c813b53cfc89cf72236f9f7176bf8ef58
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2022-12-05 14:48:19 +0530

    init files

---
+++ README.md
@@ -0,0 +1,13 @@
+a discord bot that brings gpt3chat in your discord server.
+
+Using: https://github.com/ZohaibAhmed/chatgpt-api
+
+
+after installing requirements with `pip install -r requirements.txt`
+run `playwright install` for first time.
+
+Run: `python server.py` to start your API server
+and then in different thread run `python bot.py`
+
+
+Make sure your discord keys are correct and your bot has neccessary permissions to perform action.
---
+++ bot.py
@@ -0,0 +1,28 @@
+'''RUN THIS FILE ONLY AFTER RUNNING server.py'''
+
+import discord
+import requests
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix="!", intents=intents)
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    try:
+        res = requests.get("http://localhost:5001/chat?q=" + message.content)
+        await message.channel.send(res.text)
+    except:
+        await message.channel.send("Somehting went wrong. Make sure you have logged in with your GPT3 account")
+
+
+client.run(
+    DISCORD_BOT_TOKEN)
