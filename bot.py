1b6d6645470612ce8c6145cb478da23b515e76df
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2022-12-05 15:34:18 +0530

    add commands so that it doesn't take all messages in server as input.

--- bot.py
+++ bot.py
@@ -11,20 +11,16 @@ load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix="!", intents=intents)
-client = discord.Client()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+
+@client.command()
+async def ask(ctx, *, question):
     try:
-     
-        await message.channel.trigger_typing()
-        res = requests.get("http://localhost:5001/chat?q=" + message.content)
-        await message.channel.send(res.text)
+        await ctx.trigger_typing()
+        res = requests.get("http://localhost:5001/chat?q=" + question)
+        await ctx.send(res.text)
     except:
-        await message.channel.send("Somehting went wrong. Make sure you have logged in with your GPT3 account")
-
+        await ctx.send("Somehting went wrong. Make sure you have logged in with your GPT3 account")
 
 client.run(
     DISCORD_BOT_TOKEN)
